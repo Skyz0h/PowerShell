861a76237a0eb71a2ad6dd905a37b233ef0e0616
Patrick Kolls <patrick@kollsfam.com>
2025-07-17 13:56:47 -0500

    made HTML output optional, display results to screen by default

--- Modules/DomainControllers.psm1
+++ Modules/DomainControllers.psm1
@@ -1,9 +1,9 @@
-function Test-AllDomainControllers{
+function Test-AllDomainControllers {
     [CmdletBinding()]
     param (
-        [Parameter(Position=0,mandatory=$true)]
-        [string]
-        $LogPath
+        [Parameter(Position = 0, mandatory = $false)]
+        [switch]
+        $Output
     )
     $head = @"
 <style>
@@ -53,57 +53,61 @@ function Test-AllDomainControllers{
 
 </style>
 "@
-Function Get-AllDomainControllers {
-    $allDomainControllers = Get-ADDomainController -Filter *
-    return $allDomainControllers.HostName
-}
+    Function Get-AllDomainControllers {
+        $allDomainControllers = Get-ADDomainController -Filter *
+        return $allDomainControllers.HostName
+    }
 
-$domainControllers = Get-AllDomainControllers
+    $domainControllers = Get-AllDomainControllers
 
-Function Get-DomainControllerTestResults {
-    param (
-        [Parameter( Mandatory = $false)]
-        [string]$DomainController
-    )
-    $DCDiagTest = (Dcdiag.exe /s:$DomainController /DnsBasic /skip:SystemLog ) -split ('[\r\n]')
-    $DCDiagTestResults = New-Object Object
-    $DCDiagTestResults | Add-Member -Type NoteProperty -Name "FQDN" -Value $DomainController
-    $DCDiagTest | ForEach-Object {
-        Switch -RegEx ($_) {
-            "Starting" { $TestName = ($_ -Replace ".*Starting test: ").Trim() }
-            "passed test|failed test" {
-                If ($_ -Match "passed test") {
-                    $TestStatus = "Passed"
+    Function Get-DomainControllerTestResults {
+        param (
+            [Parameter( Mandatory = $false)]
+            [string]$DomainController
+        )
+        $DCDiagTest = (Dcdiag.exe /s:$DomainController /DnsBasic /skip:SystemLog ) -split ('[\r\n]')
+        $DCDiagTestResults = New-Object Object
+        $DCDiagTestResults | Add-Member -Type NoteProperty -Name "FQDN" -Value $DomainController
+        $DCDiagTest | ForEach-Object {
+            Switch -RegEx ($_) {
+                "Starting" { $TestName = ($_ -Replace ".*Starting test: ").Trim() }
+                "passed test|failed test" {
+                    If ($_ -Match "passed test") {
+                        $TestStatus = "Passed"
+                    }
+                    Else {
+                        $TestStatus = "Failed"
+                    }
                 }
-                Else {
-                    $TestStatus = "Failed"
-                }
-            }
-        } 
-        If ($null -ne $TestName -And $null -ne $TestStatus) {
-            $DCDiagTestResults | Add-Member -Name $("$TestName".Trim()) -Value $TestStatus -Type NoteProperty -force
-            $TestName = $null; $TestStatus = $null
+            } 
+            If ($null -ne $TestName -And $null -ne $TestStatus) {
+                $DCDiagTestResults | Add-Member -Name $("$TestName".Trim()) -Value $TestStatus -Type NoteProperty -force
+                $TestName = $null; $TestStatus = $null
             }
         }
         return $DCDiagTestResults
-}
+    }
     
-[array]$allTestedDomainControllers = @()
-$report = $null
-foreach($dc in $domainControllers){
-    $results = Get-DomainControllerTestResults -DomainController $dc
-    $allTestedDomainControllers += $results
+    [array]$allTestedDomainControllers = @()
+    $report = $null
+    foreach ($dc in $domainControllers) {
+        $results = Get-DomainControllerTestResults -DomainController $dc
+        $allTestedDomainControllers += $results
 
-}
+    }
+    if ($Output) {
+        $LogPath = "$env:USERPROFILE\DomainControllerReport.html"
+        $report = $allTestedDomainControllers | ConvertTo-Html -Head $head -as Table
+        $finalReport = $report `
+            -replace '<td>Passed</td>', '<td class="pass">Passed</td>' `
+            -replace '<td>Failed</td>', '<td class="fail">Failed</td>' `
+            -replace '<table>', '<div class="container"><table class="result-table">' `
+            -replace '<tr><td><hr></td></tr>', '</table><br><br><table class="result-table">' `
 
-$report = $allTestedDomainControllers | ConvertTo-Html -Head $head -as Table
-$finalReport = $report `
-    -replace '<td>Passed</td>', '<td class="pass">Passed</td>' `
-    -replace '<td>Failed</td>', '<td class="fail">Failed</td>' `
-    -replace '<table>', '<div class="container"><table class="result-table">' `
-    -replace '<tr><td><hr></td></tr>', '</table><br><br><table class="result-table">' `
-
-$finalReport | Out-File $LogPath -Force -Confirm:$false
-Invoke-Item $LogPath
-
+        $finalReport | Out-File $LogPath -Force -Confirm:$false
+        Invoke-Item $LogPath
+    }
+    else {
+        return $allTestedDomainControllers | ft * 
+    }
 }
